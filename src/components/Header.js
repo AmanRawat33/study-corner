5a778331d417f7d70b98d7afda72e94abd806567
Aman <aman.rawat.rwd@gmail.com>
2023-12-02 01:41:30 +0530

    courselist page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-      <header className="">
+      <header>
         <div className="flex justify-between p-6 bg-slate-500">
           <h1 className="text-white font-semibold text-3xl">StudyCorner</h1>
           <button className="text-black font-semibold text-lg bg-slate-100 px-4 py-2 rounded-md" >My Dashboard</button>
